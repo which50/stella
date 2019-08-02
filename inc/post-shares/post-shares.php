642145fcbfd34adc64304cff22cfbe31f70e566a
Martin Ilagan <martin.ilagan@news.com.au>
2019-08-02 14:29:37 +1000

    Check page type as posts seem incorrect

--- inc/post-shares/post-shares.php
+++ inc/post-shares/post-shares.php
@@ -12,36 +12,35 @@ if ( ! defined( 'VW_CONST_POST_SHARES_META_KEY' ) ) define( 'VW_CONST_POST_SHARE
 // }
 
 function wpse8170_loop() {
-    global $wp_query;
-    $loop = 'notfound';
+	$loop = 'not found';
 
-    if ( $wp_query->is_page ) {
+    if ( is_page() ) {
         $loop = is_front_page() ? 'front' : 'page';
-    } elseif ( $wp_query->is_home ) {
+    } elseif ( is_home() ) {
         $loop = 'home';
-    } elseif ( $wp_query->is_single ) {
-        $loop = ( $wp_query->is_attachment ) ? 'attachment' : 'single';
-    } elseif ( $wp_query->is_category ) {
+    } elseif ( is_single() ) {
+        $loop = ( is_attachment () ) ? 'attachment' : 'single';
+    } elseif ( is_category() ) {
         $loop = 'category';
-    } elseif ( $wp_query->is_tag ) {
+    } elseif ( is_tag() ) {
         $loop = 'tag';
-    } elseif ( $wp_query->is_tax ) {
+    } elseif ( is_tax() ) {
         $loop = 'tax';
-    } elseif ( $wp_query->is_archive ) {
-        if ( $wp_query->is_day ) {
+    } elseif ( is_archive() ) {
+        if ( is_day() ) {
             $loop = 'day';
-        } elseif ( $wp_query->is_month ) {
+        } elseif ( is_month() ) {
             $loop = 'month';
-        } elseif ( $wp_query->is_year ) {
+        } elseif ( is_year() ) {
             $loop = 'year';
-        } elseif ( $wp_query->is_author ) {
+        } elseif ( is_author() ) {
             $loop = 'author';
         } else {
             $loop = 'archive';
         }
-    } elseif ( $wp_query->is_search ) {
+    } elseif ( is_search() ) {
         $loop = 'search';
-    } elseif ( $wp_query->is_404 ) {
+    } elseif ( is_404() ) {
         $loop = 'notfound';
     }
 
