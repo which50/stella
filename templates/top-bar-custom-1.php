fcfd413f47eb8ebb502b61da6d5f23b4997c68b3
Martin Ilagan <martin.ilagan@news.com.au>
2019-08-14 13:08:04 +1000

    Test comment

--- templates/top-bar-custom-1.php
+++ templates/top-bar-custom-1.php
@@ -1,6 +1,12 @@
-<?php
-/* -----------------------------------------------------------------------------
- * This is a sample file for custom layout. You can edit this file to have your own layout.
- * -------------------------------------------------------------------------- */
-include( 'top-bar-menu-social.php' );
+<?php
+
+/* -----------------------------------------------------------------------------
+
+ * This is a sample file for custom layout. You can edit this file to have your own layout.
+
+ * -------------------------------------------------------------------------- */
+
+echo '<!-- TEST COMMENT -->';
+include( 'top-bar-menu-social.php' );
+
 ?>
