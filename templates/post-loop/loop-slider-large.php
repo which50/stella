3bbdc74a49346c5f20d9df607fa985f69901942a
Martin Ilagan <martin.ilagan@news.com.au>
2019-08-20 12:20:59 +1000

    Test comment

--- templates/post-loop/loop-slider-large.php
+++ templates/post-loop/loop-slider-large.php
@@ -1,23 +1,45 @@
-<?php
-global $vw_secondary_query;
-if ( empty( $vw_secondary_query ) ) return;
-?>
-
-<div class="vw-loop vw-loop--slider vw-loop--slider-large">
-	<div class="clearfix"></div>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<div class="swiper-container vw-swiper-large-nav">
-					<div class="swiper-wrapper">
-					<?php while ( $vw_secondary_query->have_posts() ) : $vw_secondary_query->the_post(); ?>
-						<div class="swiper-slide">
-							<?php get_template_part( 'templates/post-loop/post-slide-large', get_post_format() ); ?>
-						</div>
-					<?php endwhile; ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<?php
+
+global $vw_secondary_query;
+
+if ( empty( $vw_secondary_query ) ) return;
+
+?>
+
+
+
+<div class="vw-loop vw-loop--slider vw-loop--slider-large marto">
+
+	<div class="clearfix"></div>
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-sm-12">
+
+				<div class="swiper-container vw-swiper-large-nav">
+
+					<div class="swiper-wrapper">
+
+					<?php while ( $vw_secondary_query->have_posts() ) : $vw_secondary_query->the_post(); ?>
+
+						<div class="swiper-slide">
+
+							<?php get_template_part( 'templates/post-loop/post-slide-large', get_post_format() ); ?>
+
+						</div>
+
+					<?php endwhile; ?>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
 </div>
