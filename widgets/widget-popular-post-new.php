51bac3f18a8f3669b88ffe483f3bb4ba757ae72d
Martin Ilagan <martin.ilagan@news.com.au>
2019-08-29 11:42:06 +1000

    Test

--- widgets/widget-popular-post-new.php
+++ widgets/widget-popular-post-new.php
@@ -26,16 +26,16 @@ if ( ! class_exists( 'Vw_widget_popular_post' ) ) {
 		function widget( $instance ) {
 			$count = intval( $instance['count'] );
 
-			echo '<!-- <?php var_dump( $instance ); ?> -->';
+			echo '<div style="display: none;"><?php var_dump( $instance ); ?></div>';
 
 			if ( function_exists( 'icl_t' ) ) {
 				$instance['title'] = icl_t( VW_THEME_NAME.' Widget', $this->id.'_title', $instance['title'] );
-				echo '<!-- TITLE 1 -->';
+				echo '<div style="display: none;">TITLE 1</div>';
 			}
 
 			if ( ! empty( $instance['title'] ) ) {
 				$title = apply_filters( 'widget_title', wp_kses_data( $instance['title'] ), $instance, $this->id_base);
-				echo '<!-- TITLE 2 -->';
+				echo '<div style="display: none;">TITLE 2</div>';
 			}
 
 			echo $before_widget;
@@ -46,7 +46,7 @@ if ( ! class_exists( 'Vw_widget_popular_post' ) ) {
 
 			?>
 
-			<div id="most-popular-list" class="tabs tabbed-list">
+			<div id="most-popular-list" class="tabs tabbed-lists">
 				<div class="tab">
 					<input type="radio" id="tab-1" name="tab-group-1" checked>
 					<label id="tab-label-1" for="tab-1">Week</label>
